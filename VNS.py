d780cfdf8f1fe9d6a351a90ae6156dfeacac50da
calebchadz <calebchadz@gmail.com>
2020-07-23 22:09:41 +1200

    added all rect related functions to the Rectangle file

--- VNS.py
+++ VNS.py
@@ -2,20 +2,6 @@ import csv
 import sys
 import Rectangle
 
-# Function for reading in a CSV file and translatng to Rectangle objects
-def get_rectangles(file_name):
-    rectangles = []
-    with open(file_name, 'r', encoding='utf-8-sig') as csv_file:
-        reader = csv.reader(csv_file)
-        line_count = 0
-        for row in reader:
-            if line_count == 0:
-                # print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                rectangles.append(Rectangle(row[0], row[1], row[2], 0,0))
-    return rectangles
-
 # Main method
 file_name = ''
 # get the command line arguments
@@ -24,8 +10,6 @@ if len(sys.argv) == 2:
     print(file_name)
 # parse in the csv to rectangle objects
 rectangles = {}
-rectangles = get_rectangles(file_name)
+rectangles = Rectangle.get_rectangles(file_name)
 for rect in rectangles:
     rect.print()
-
-

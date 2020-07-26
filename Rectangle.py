a67134285972e678990062d05d54532c86aa22e0
calebchadz <calebchadz@gmail.com>
2020-07-27 03:43:05 +1200

    refactored functions out into the rectangle class.

--- Rectangle.py
+++ Rectangle.py
@@ -1,4 +1,5 @@
 import csv
+from graphics import *
 
 # A rectangle object to represent the rectangles in out problem.
 class Rect:
@@ -12,7 +13,7 @@ class Rect:
     def print(self):
         print("ID: " + str(self.id) + "\nDimensions: (" + str(self.width) + ", " + str(self.height) + ")" + "\nCoordinates: (" + str(self.xpos) + ", " + str(self.ypos) + ")")
 
-# Function fo
+# Function to read in a CSV file of rectangle dimensions to be stored as rectangle objects.
 def get_rectangles(file_name):
     rectangles = []
     with open(file_name, 'r', encoding='utf-8-sig') as csv_file:
@@ -26,3 +27,23 @@ def get_rectangles(file_name):
                 # add the new rectangle, make sure correct types eg integer.
                 rectangles.append(Rect(row[0], int(row[1]), int(row[2]), 0,0))
     return rectangles
+
+#function to draw out the final solution to a window to be viewed.
+def draw_solution(solution, solution_height):
+    win = GraphWin("Solution Window", 400, solution_height)
+    # set background to black
+    win.setBackground(color_rgb(150,200,210))
+    # For each rectangle in the list
+    for r in solution:
+        #get the position of this rectangle and create a drawable.
+        pt1 = Point(r.xpos, r.ypos)
+        pt2 = Point((r.xpos + r.width), (r.ypos + r.height))
+        rect_draw  = Rectangle(pt1, pt2)
+        # set the outline color
+        rect_draw.setFill(color_rgb(199,0,57))
+        # draw it to the window
+        rect_draw.draw(win)
+    # wait for a click then close the program
+    win.getMouse()
+    win.close()
+    return
--- VNS.py
+++ VNS.py
@@ -22,22 +22,5 @@ def main():
     # Generate initial solution, represented as a list of boxes and thier coordinates.
 
     # Draw out the final solution
-    win = GraphWin("Solution Window", 400, solution_height)
-    # set background to black
-    win.setBackground(color_rgb(150,200,210))
-    # For each rectangle in the list
-    for r in rectangles:
-        #get the position of this rectangle and create a drawable.
-        pt1 = Point(r.xpos, r.ypos)
-        pt2 = Point((r.xpos + r.width), (r.ypos + r.height))
-        rect_draw  = Rectangle(pt1, pt2)
-        # set the outline color
-        rect_draw.setFill(color_rgb(199,0,57))
-        # draw it to the window
-        rect_draw.draw(win)
-    # wait for a click then close the program
-    win.getMouse()
-    win.close()
+    draw_solution(rectangles, solution_height)  
 main()
-
-

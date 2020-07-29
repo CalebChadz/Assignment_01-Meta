593c8bdbfcca530b6f115d08323e000f4c6387a9
calebchadz <calebchadz@gmail.com>
2020-07-30 01:36:19 +1200

    NO overlapping solutions!!

--- VNS.py
+++ VNS.py
@@ -7,45 +7,44 @@ from graphics import *
 
 # Global Variables
 MAX_WIDTH = 400
-CURR_HEIGHT = 0
 solution_width = 400
 solution_matrix = [ [0] * MAX_WIDTH for i in range(1)]
 
 def initial_solution(rectangle_list):
         init_x = 0
         init_y = 0
-        global CURR_HEIGHT
         global solution_matrix
         for rect in rectangle_list:
             # always recalculate the starting height
             init_y = 0
             #check to see if this row is full, if yes start from left.
-            if (init_x + rect.width) > (MAX_WIDTH - 1):
+            if (init_x + rect.width) > (MAX_WIDTH-1):
                 init_x = 0
-            #move up to the first free height
-            while(solution_matrix[init_y][init_x] != 0):
-                init_y += 1
-                if(CURR_HEIGHT < init_y):
-                    solution_matrix.append([0] * MAX_WIDTH) 
-                    CURR_HEIGHT += 1
-            #now check the right side of rectangle isnt colliding
-            init_x = init_x + rect.width
-            while(solution_matrix[init_y][init_x - 1] != 0):
-                init_y += 1
-                if(CURR_HEIGHT < init_y):
-                    solution_matrix.append([0] * MAX_WIDTH) 
-                    CURR_HEIGHT += 1
+            # check the bottom of rectangle is not colliding with any other rectangle for all side length
+            ypos = 0
+            while (ypos < rect.height):
+                if(len(solution_matrix) - 1 < init_y + ypos):
+                            solution_matrix.append([0] * MAX_WIDTH)
+                xpos = 0
+                while (xpos < rect.width):
+                    if (solution_matrix[init_y + ypos][init_x + xpos] != 0):
+                        init_y += 1
+                        xpos = 0
+                        ypos = 0
+                        if(len(solution_matrix) - 1 < init_y):
+                            solution_matrix.append([0] * MAX_WIDTH) 
+                    else:
+                        xpos += 1
+                ypos += 1
             #now we have a free space to draw the rect
-            rect.xpos = init_x - rect.width
+            rect.xpos = init_x 
             rect.ypos = init_y
             # fill in its matrix space
             for x in range(rect.width):
                 for y in range(rect.height):
-                    if (rect.ypos + y) > CURR_HEIGHT:
-                        CURR_HEIGHT += 1
-                        solution_matrix.append([0] * MAX_WIDTH) 
                     solution_matrix[rect.ypos + y][rect.xpos + x] = rect.id
-        return CURR_HEIGHT
+            init_x += rect.width
+        return len(solution_matrix) - 1
 
 # Main method
 def main():
@@ -61,4 +60,5 @@ def main():
     print("solution height: " + str(solution_height))
     # Draw out the final solution
     draw_solution(rectangles, solution_width, solution_height)  
+    # print(solution_matrix)
 main()
